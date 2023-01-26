9051484e447262602d1ab9f29239885e85bc8fc1
CameronOwens <camowens58@gmail.com>
2023-01-25 23:50:26 -0500

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+            var actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
